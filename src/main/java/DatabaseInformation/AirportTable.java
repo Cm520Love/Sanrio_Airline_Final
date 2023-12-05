7d5f62ecf8a1a9d6685e51f957e77410a5ba548e
Sunny <cherrycheesecakeplease@gmail.com>
2023-12-05 15:58:03 -0500

    final touches

--- src/main/java/DatabaseInformation/AirportTable.java
+++ src/main/java/DatabaseInformation/AirportTable.java
@@ -11,7 +11,9 @@ public class AirportTable implements InitializeDatabase{
     }
 
     public void initialize() throws Exception {
+        // get the information in csv file
         Scanner input = new Scanner(new File("AirportInformation.csv"));
+
         // connect to db
         String url = "jdbc:sqlserver://test-sunnysqlserver.database.windows.net:1433;database=Sanrio_Airlines;user=stinkysnoopy@test-sunnysqlserver;password=AdminPass!!!;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
         Connection conn = DriverManager.getConnection(url);
@@ -23,12 +25,18 @@ public class AirportTable implements InitializeDatabase{
         // skips over the column labels
         input.nextLine();
 
+        // while the file still has information
         while (input.hasNext()) {
+            // get each line, split it using ,
             String word = input.nextLine();
             String[] stuff = word.split(",");
+
+            // put each part in the array into the prepared statement
             for (int index = 1, index2 = 0; index <= 4; index++, index2++) {
                 pStatement.setString(index, stuff[index2]);
             }
+
+            // place that single row of airport information into the db
             pStatement.execute();
         }
     }
